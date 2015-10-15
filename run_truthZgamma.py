dba47fa731732ef20bc40b8151b95d222950ecce
Russell Smith <rsmith@cern.ch>
2015-10-15 12:03:05 -0400

    using dict instead two lists

--- run_truthZgamma.py
+++ run_truthZgamma.py
@@ -122,17 +122,16 @@ for mysamplehandlername in sh_bg.keys():
 	job = ROOT.EL.Job()
 	job.sampleHandler(mysamplehandler)
 
-	baseline_cuts = []#[]
-	baseline_cuts += ["met>140"]
-	baseline_cuts += ["NTRJigsawVars.RJVars_SS_MDeltaR/1000.>300"   ]
-#        baseline_cuts += ["Length%(jetPt) >= 2"]
-#        baseline_cuts += ["jetPt[0] > 50"]
-#        baseline_cuts += ["jetPt[1] > 50"]
-        baseline_cuts += ["NTRJigsawVars.RJVars_QCD_Rpt<0.4"]
-        baseline_cuts += ["NTRJigsawVars.RJVars_QCD_Delta1>.05 * (1 - NTRJigsawVars.RJVars_QCD_Rpsib)"]
+	baseline_cuts = {}#[]
+	baseline_cuts["met>140"]                                  = [50,0,1000]
+	baseline_cuts["NTRJigsawVars.RJVars_SS_MDeltaR/1000.>300"]= [50,0,2000]
+#       baseline_cuts["Length%(jetPt) >= 2"]
+#       baseline_cuts["jetPt[0] > 50"]
+#       baseline_cuts["jetPt[1] > 50"]
+        baseline_cuts["NTRJigsawVars.RJVars_QCD_Rpt<0.4"]         =  [50,0,1]
+        baseline_cuts["NTRJigsawVars.RJVars_QCD_Delta1>.05 * (1 - NTRJigsawVars.RJVars_QCD_Rpsib)"] = [50,-1,1]
 
-        cry_cuts = []
-        cry_cuts.extend(baseline_cuts)
+        cry_cuts = baseline_cuts
 
 #        cry_cuts += ["NTRJigsawVars.RJVars_QCD_Delta1>*NTRJigsawVars.RJVars_QCD_Rpsib>-0.7"   ]
 	#
@@ -153,53 +152,52 @@ for mysamplehandlername in sh_bg.keys():
 	# cry_cuts += ["NTRJigsawVars.RJVars_SS_VisShape>0.1"   ]
 	# cry_cuts += ["NTRJigsawVars.RJVars_MG/1000.>800"   ]
 
-	cry_limits = []
-	cry_limits += [ (50,0,1000) ]  #["met>100"]
-	cry_limits += [ (50,0,2000) ]  #["NTRJigsawVars.RJVars_SS_MDeltaR/1000.>300"   ]
-	cry_limits += [ (50,0,500) ]  #["NTRJigsawVars.RJVars_G_0_Jet1_pT/1000.>150."   ]
-	cry_limits += [ (50,0,500) ]  #["NTRJigsawVars.RJVars_G_1_Jet1_pT/1000.>150."   ]
-	cry_limits += [ (50,0,500) ]  #["NTRJigsawVars.RJVars_G_0_Jet2_pT/1000.>110."   ]
-	cry_limits += [ (50,0,500) ]  #["NTRJigsawVars.RJVars_G_1_Jet2_pT/1000.>110."   ]
-	cry_limits += [ (50,0,1) ]  #["abs(NTRJigsawVars.RJVars_SS_CosTheta)<0.9"   ]
-	cry_limits += [ (50,0,1) ]  #["NTRJigsawVars.RJVars_G_0_PInvHS>0.25 && NTRJigsawVars.RJVars_G_0_PInvHS<1"   ]
-	cry_limits += [ (50,0,1) ]  #["NTRJigsawVars.RJVars_G_1_PInvHS>0.25 && NTRJigsawVars.RJVars_G_0_PInvHS<1"   ]
-	cry_limits += [ (50,-1,1) ]  #["NTRJigsawVars.RJVars_G_0_CosTheta>-0.7 && NTRJigsawVars.RJVars_G_0_CosTheta<0.7"   ]
-	cry_limits += [ (50,-1,1) ]  #["NTRJigsawVars.RJVars_G_1_CosTheta>-0.7 && NTRJigsawVars.RJVars_G_0_CosTheta<0.7"   ]
-	cry_limits += [ (50,-1,1) ]  #["NTRJigsawVars.RJVars_C_0_CosTheta>-0.75 && NTRJigsawVars.RJVars_C_0_CosTheta<0.8"   ]
-	cry_limits += [ (50,-1,1) ]  #["NTRJigsawVars.RJVars_C_1_CosTheta>-0.75 && NTRJigsawVars.RJVars_C_0_CosTheta<0.8"   ]
-	cry_limits += [ (50,-1,1) ]  #["cos(NTRJigsawVars.RJVars_G_0_dPhiGC)>-0.8 && cos(NTRJigsawVars.RJVars_G_0_dPhiGC)<0.7"]
-	cry_limits += [ (50,-1,1) ]  #["cos(NTRJigsawVars.RJVars_G_1_dPhiGC)>-0.8 && cos(NTRJigsawVars.RJVars_G_1_dPhiGC)<0.7"]
-	cry_limits += [ (50,0,1) ]  #["NTRJigsawVars.RJVars_DeltaBetaGG>0.2" ]
-	cry_limits += [ (50,0,4) ]  #["NTRJigsawVars.RJVars_dphiVG>0.3 && NTRJigsawVars.RJVars_dphiVG<2.7"   ]
-	cry_limits += [ (50,0,1) ]  #["NTRJigsawVars.RJVars_QCD_Rpt<0.3"]
-	cry_limits += [ (50,-1,1) ]  #["NTRJigsawVars.RJVars_QCD_Delta1*NTRJigsawVars.RJVars_QCD_Rpsib>-0.7"   ]
-	cry_limits += [ (50,0,1) ]  #visshape
-	cry_limits += [ (50,0,2000) ]  #["NTRJigsawVars.RJVars_QCD_Delta1*NTRJigsawVars.RJVars_QCD_Rpsib>-0.7"   ]
-
+	# cry_limits = []
+	# cry_limits += [ (50,0,1000) ]  #["met>100"]
+	# cry_limits += [ (50,0,2000) ]  #["NTRJigsawVars.RJVars_SS_MDeltaR/1000.>300"   ]
+	# cry_limits += [ (50,0,500) ]  #["NTRJigsawVars.RJVars_G_0_Jet1_pT/1000.>150."   ]
+	# cry_limits += [ (50,0,500) ]  #["NTRJigsawVars.RJVars_G_1_Jet1_pT/1000.>150."   ]
+	# cry_limits += [ (50,0,500) ]  #["NTRJigsawVars.RJVars_G_0_Jet2_pT/1000.>110."   ]
+	# cry_limits += [ (50,0,500) ]  #["NTRJigsawVars.RJVars_G_1_Jet2_pT/1000.>110."   ]
+	# cry_limits += [ (50,0,1) ]  #["abs(NTRJigsawVars.RJVars_SS_CosTheta)<0.9"   ]
+	# cry_limits += [ (50,0,1) ]  #["NTRJigsawVars.RJVars_G_0_PInvHS>0.25 && NTRJigsawVars.RJVars_G_0_PInvHS<1"   ]
+	# cry_limits += [ (50,0,1) ]  #["NTRJigsawVars.RJVars_G_1_PInvHS>0.25 && NTRJigsawVars.RJVars_G_0_PInvHS<1"   ]
+	# cry_limits += [ (50,-1,1) ]  #["NTRJigsawVars.RJVars_G_0_CosTheta>-0.7 && NTRJigsawVars.RJVars_G_0_CosTheta<0.7"   ]
+	# cry_limits += [ (50,-1,1) ]  #["NTRJigsawVars.RJVars_G_1_CosTheta>-0.7 && NTRJigsawVars.RJVars_G_0_CosTheta<0.7"   ]
+	# cry_limits += [ (50,-1,1) ]  #["NTRJigsawVars.RJVars_C_0_CosTheta>-0.75 && NTRJigsawVars.RJVars_C_0_CosTheta<0.8"   ]
+	# cry_limits += [ (50,-1,1) ]  #["NTRJigsawVars.RJVars_C_1_CosTheta>-0.75 && NTRJigsawVars.RJVars_C_0_CosTheta<0.8"   ]
+	# cry_limits += [ (50,-1,1) ]  #["cos(NTRJigsawVars.RJVars_G_0_dPhiGC)>-0.8 && cos(NTRJigsawVars.RJVars_G_0_dPhiGC)<0.7"]
+	# cry_limits += [ (50,-1,1) ]  #["cos(NTRJigsawVars.RJVars_G_1_dPhiGC)>-0.8 && cos(NTRJigsawVars.RJVars_G_1_dPhiGC)<0.7"]
+	# cry_limits += [ (50,0,1) ]  #["NTRJigsawVars.RJVars_DeltaBetaGG>0.2" ]
+	# cry_limits += [ (50,0,4) ]  #["NTRJigsawVars.RJVars_dphiVG>0.3 && NTRJigsawVars.RJVars_dphiVG<2.7"   ]
+	# cry_limits += [ (50,0,1) ]  #["NTRJigsawVars.RJVars_QCD_Rpt<0.3"]
+	# cry_limits += [ (50,-1,1) ]  #["NTRJigsawVars.RJVars_QCD_Delta1*NTRJigsawVars.RJVars_QCD_Rpsib>-0.7"   ]
+	# cry_limits += [ (50,0,1) ]  #visshape
+	# cry_limits += [ (50,0,2000) ]  #["NTRJigsawVars.RJVars_QCD_Delta1*NTRJigsawVars.RJVars_QCD_Rpsib>-0.7"   ]
 
 	## Define your cut strings here....
 	cuts = {
 		# "no_cut": "(NTRJigsawVars.RJVars_G_0_Jet1_pT/1000. > 250)",
 		# "l1trigger": "(NTVars.nJet>1 && met > 100)",
 #		"hlttrigger": "(NTVars.nJet>1 && met > 100)*(NTRJigsawVars.RJVars_SS_MDeltaR/1000.>300)",
-#		"sirop_1200_800": "*".join( ["(%s)"%mycut for mycut in sirop_1200_800_cuts ]),
-		"sirop_tight": "*".join( ["(%s)"%mycut for mycut in cry_cuts ])
+#		"cry_1200_800": "*".join( ["(%s)"%mycut for mycut in cry_1200_800_cuts ]),
+		"cry_tight": "*".join( ["(%s)"%mycut for mycut in cry_cuts.keys() ])
 	}
 
 
 	#################################################################################################
 
-	## This part sets up both N-1 hists and the cutflow histogram for "sirop_tight"
+	## This part sets up both N-1 hists and the cutflow histogram for "cry_tight"
 
-	cutflow = ROOT.TH1F ("cutflow", "cutflow", len(cry_cuts)+1 , 0, len(cry_cuts)+1 );
+	cutflow = ROOT.TH1F ("cutflow", "cutflow", len(cry_cuts.keys())+1 , 0, len(cry_cuts.keys())+1 );
 	cutflow.GetXaxis().SetBinLabel (1, "NTVars.eventWeight");
 
-	for i,cutpart in enumerate(cry_cuts):
+	for i,cutpart in enumerate(cry_cuts.keys()):
 
 		cutpartname = cutpart.split("/")[0].replace("*","x").split("<")[0].split(">")[0]
-		job.algsAdd (ROOT.MD.AlgHist(ROOT.TH1F("sirop_tight_minus_%s"%cutpartname, "sirop_tight_%s"%cutpartname, cry_limits[i][0], cry_limits[i][1], cry_limits[i][2] ),
+		job.algsAdd (ROOT.MD.AlgHist(ROOT.TH1F("cry_tight_minus_%s"%cutpartname, "cry_tight_%s"%cutpartname, cry_cuts.values()[i][0], cry_cuts.values()[i][1], cry_cuts.values()[i][2] ),
 			cutpart.split("<")[0].split(">")[0],
-			"NTVars.eventWeight*%s"%("*".join( ["(%s)"%mycut for mycut in cry_cuts if  mycut!=cutpart ]))    )        )
+			"NTVars.eventWeight*%s"%("*".join( ["(%s)"%mycut for mycut in cry_cuts.keys() if  mycut!=cutpart ]))    )        )
 
 		cutflow.GetXaxis().SetBinLabel (i+2, cutpart);
 
