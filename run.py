9f3eea03218beb4da4a364724d699f3ca466f95f
lawrenceleejr <lawrenceleejr@gmail.com>
2015-10-18 11:17:47 +0200

    compatibility with new ntuples with precalculated vars

--- run.py
+++ run.py
@@ -47,7 +47,7 @@ for sampleHandlerName in [
 						"GG_direct",
 							]:
 
-	f = ROOT.TFile("%s/%s.root"%(directory,sampleHandlerName) )
+	f = ROOT.TFile("%s/../%s.root"%(directory,sampleHandlerName) )
 	treeList = []
 	dirList = ROOT.gDirectory.GetListOfKeys()
 	for k1 in dirList: 
@@ -66,33 +66,25 @@ for sampleHandlerName in [
 cuts = {}
 limits = {}
 
-
-var_dphiVP = "((dphiVP-pi)/(pi))"
-var_dphiPCa = "( dphiPCa*(dphiPCa<pi)+(2*pi-dphiPCa)*(dphiPCa>pi) )"
-var_dphiPCb = "( dphiPCb*(dphiPCb<pi)+(2*pi-dphiPCb)*(dphiPCb>pi) )"
-var_HT6PP = "(pTPP_V1a+pTPP_V2a+pTPP_V1b+pTPP_V2b+pTPP_Ia+pTPP_Ib)"
-var_RPZ = " RPZ / (RPZ+%s)  "%var_HT6PP
-
-
 cuts["SR1"] = []
-cuts["SR1"] += ["(pT_jet1 > 50.)"]
-cuts["SR1"] += ["(pT_jet2 > 50.)"]
-cuts["SR1"] += ["(MDR > 300.)"]
-cuts["SR1"] += ["(deltaQCD/(Rsib-1) < 0.05)"]
-cuts["SR1"] += ["(RPT < 0.4)"]
-cuts["SR1"] += ["(%s < 0.5)"%var_RPZ]
-cuts["SR1"] += ["(abs(%s) < 0.55)"%var_dphiVP ]
-cuts["SR1"] += ["(abs(cosP+%s)/2 < 0.3)"%var_dphiVP]
-cuts["SR1"] += ["( abs(%s-%s)/pi < 0.7  )"%(var_dphiPCa,var_dphiPCb)]
-cuts["SR1"] += ["((%s+%s)/(2*pi) < 0.6  )"%(var_dphiPCa,var_dphiPCb) ]
-cuts["SR1"] += ["((H2PP / H6PP) > 0.3)" ] 
-cuts["SR1"] += ["((%s / H6PP) > 0.3)"%var_HT6PP ] 
-cuts["SR1"] += ["((H2PP/%s) < 0.85)"%var_HT6PP ] 
-cuts["SR1"] += ["(abs(H2Pa/H3Pa - H2Pb/H3Pb) < 0.35 )" ] 
-cuts["SR1"] += ["(  %s > 1000.  )"%var_HT6PP  ]
-cuts["SR1"] += ["(  H2PP > 500.  )"  ]
-cuts["SR1"] += ["(  min(H3Pa,H3Pb) > 350. )"  ]
-cuts["SR1"] += ["(  min(pTPP_jet2a,pTPP_jet2b)/%s > 0.06 )"%var_HT6PP  ]
+cuts["SR1"] += ["( pT_jet1 > 50.)"]
+cuts["SR1"] += ["( pT_jet2 > 50.)"]
+cuts["SR1"] += ["( MDR > 300.)"]
+cuts["SR1"] += ["( deltaQCD/(Rsib-1) < 0.05)"]
+cuts["SR1"] += ["( RPT < 0.4)"]
+cuts["SR1"] += ["( RPZ_HT6PP < 0.5)"]
+cuts["SR1"] += ["( abs(dphiVP) < 0.55)" ]
+cuts["SR1"] += ["( sangle < 0.3)"]
+cuts["SR1"] += ["( abs(ddphiPC) < 0.7  )"]
+cuts["SR1"] += ["( sdphiPC < 0.6  )"]
+cuts["SR1"] += ["( R_H2PP_H6PP > 0.3)" ] 
+cuts["SR1"] += ["( R_HT6PP_H6PP > 0.3)" ] 
+cuts["SR1"] += ["( R_H2PP_HT6PP < 0.85)" ] 
+cuts["SR1"] += ["( abs(dH2o3P) < 0.35 )" ] 
+cuts["SR1"] += ["( HT6PP > 1000.  )"  ]
+cuts["SR1"] += ["( H2PP > 500.  )"  ]
+cuts["SR1"] += ["( minH3P > 350. )"  ]
+cuts["SR1"] += ["( min(R_pTj2a_HT6PP,R_pTj2b_HT6PP) > 0.06 )"  ]
 
 
 
@@ -107,10 +99,10 @@ limits["SR1"] +=  [(50,0,1)]  # ["(abs(%s)<0.55)"%var_dphiVP ]
 limits["SR1"] +=  [(50,0,1)]  # ["(abs(cosP+%s)/2<0.3)"%var_dphiVP]
 limits["SR1"] +=  [(50,0,1)]  # ["( abs(var_dphiPCa-var_dphiPCb) < 0.7  )"]
 limits["SR1"] +=  [(50,0,1)]  # ["(var_dphiPCa+var_dphiPCb < 0.6  )"]
-limits["SR1"] +=   [(50,0,1)]       # ["((H2PP / H6PP) > 0.3)" ] 
-limits["SR1"] +=   [(50,0,1)]       # ["((%s / H6PP) > 0.3)"%var_HT6PP ] 
-limits["SR1"] +=   [(50,0,1)]       # ["((H2PP/%s) < 0.85)"%var_HT6PP ] 
-limits["SR1"] +=   [(50,0,1)]       # ["(abs(H2Pa/H3Pa - H2Pb/H3Pb) < 0.35" ] 
+limits["SR1"] +=  [(50,0,1)]       # ["((H2PP / H6PP) > 0.3)" ] 
+limits["SR1"] +=  [(50,0,1)]       # ["((%s / H6PP) > 0.3)"%var_HT6PP ] 
+limits["SR1"] +=  [(50,0,1)]       # ["((H2PP/%s) < 0.85)"%var_HT6PP ] 
+limits["SR1"] +=  [(50,0,1)]       # ["(abs(H2Pa/H3Pa - H2Pb/H3Pb) < 0.35" ] 
 limits["SR1"] +=  [(50,0,4000)]  # ["(  %s > 1000  )"%var_HT6PP  ]
 limits["SR1"] +=  [(50,0,4000)]  # ["(  H2PP > 500  )"  ]
 limits["SR1"] +=  [(50,0,2000)]  # ["(  min(H3Pa,H3pb) > 350 )"  ]
@@ -119,24 +111,24 @@ limits["SR1"] +=  [(50,0,0.2)]  # ["(  min(pTPP_jet2a,pTPP_jet2b)/%s > 350 )"%va
 
 
 cuts["SR2"] = []
-cuts["SR2"] += ["(pT_jet1 > 50.)"]
-cuts["SR2"] += ["(pT_jet2 > 50.)"]
-cuts["SR2"] += ["(MDR > 300.)"]
-cuts["SR2"] += ["(deltaQCD/(Rsib-1) < 0.05)"]
-cuts["SR2"] += ["(RPT < 0.4)"]
-cuts["SR2"] += ["(%s < 0.5)"%var_RPZ]
-cuts["SR2"] += ["(abs(cosP+%s)/2 < 0.5)"%var_dphiVP]
-cuts["SR2"] += ["(abs(cosP-%s)/2 < 0.5)"%var_dphiVP]
-cuts["SR2"] += ["( abs(%s-%s)/pi  < 0.7  )"%(var_dphiPCa,var_dphiPCb)]
-cuts["SR2"] += ["( (%s+%s)/(2*pi)  < 0.6  )"%(var_dphiPCa,var_dphiPCb) ]
-cuts["SR2"] += ["((H2PP / H6PP) > 0.3)" ] 
-cuts["SR2"] += ["((%s / H6PP) > 0.75)"%var_HT6PP ] 
-cuts["SR2"] += ["((H2PP/%s) < 0.9)"%var_HT6PP ] 
-cuts["SR2"] += ["(abs(H2Pa/H3Pa - H2Pb/H3Pb) < 0.35)" ] 
-cuts["SR2"] += ["(  %s > 1100.  )"%var_HT6PP  ]
-cuts["SR2"] += ["(  H2PP > 600.  )"  ]
-cuts["SR2"] += ["(  min(H3Pa,H3Pb) > 400. )"  ]
-cuts["SR2"] += ["(  min(pTPP_jet2a,pTPP_jet2b)/%s > 0.06 )"%var_HT6PP  ]
+cuts["SR2"] += ["( pT_jet1 > 50. )"]
+cuts["SR2"] += ["( pT_jet2 > 50. )"]
+cuts["SR2"] += ["( MDR > 300. )"]
+cuts["SR2"] += ["( deltaQCD/(Rsib-1 ) < 0.05 )"]
+cuts["SR2"] += ["( RPT < 0.4 )"]
+cuts["SR2"] += ["( RPZ_HT6PP < 0.5 )"]
+cuts["SR2"] += ["( sangle < 0.5 )"]
+cuts["SR2"] += ["( abs(dangle ) < 0.5 )"]
+cuts["SR2"] += ["( abs(ddphiPC ) < 0.7   )"]
+cuts["SR2"] += ["( sdphiPC  < 0.6   )" ]
+cuts["SR2"] += ["( R_H2PP_H6PP > 0.3 )" ] 
+cuts["SR2"] += ["( R_HT6PP_H6PP > 0.75 )" ] 
+cuts["SR2"] += ["( R_H2PP_HT6PP < 0.9 )" ] 
+cuts["SR2"] += ["( abs(dH2o3P ) < 0.35 )" ] 
+cuts["SR2"] += ["( HT6PP > 1100.   )"  ]
+cuts["SR2"] += ["( H2PP > 600.   )"  ]
+cuts["SR2"] += ["( minH3P > 400.  )"  ]
+cuts["SR2"] += ["( min(R_pTj2a_HT6PP,R_pTj2b_HT6PP) > 0.06  )"  ]
 
 
 limits["SR2"] = []
@@ -162,21 +154,21 @@ limits["SR2"] += [(50,0,0.2)]  #["(  min(pTPP_jet2a,pTPP_jet2b)/%s > 0.06 )"%var
 
 
 cuts["SR3"] = []
-cuts["SR3"] += ["(pT_jet1 > 50.)"]
-cuts["SR3"] += ["(pT_jet2 > 50.)"]
-cuts["SR3"] += ["(MDR > 300.)"]
-cuts["SR3"] += ["(deltaQCD/(Rsib-1) < 0.05)"]
-cuts["SR3"] += ["(RPT < 0.4)"]
-cuts["SR3"] += ["(%s < 0.55)"%var_RPZ]
-cuts["SR3"] += ["(abs(cosP+%s)/2 < 0.3)"%var_dphiVP]
-cuts["SR3"] += ["((H2PP / H6PP) > 0.23)" ] 
-cuts["SR3"] += ["((%s / H6PP) > 0.55)"%var_HT6PP ] 
-cuts["SR3"] += ["(  min(H3Pa,H3Pb)/H6PP > 0.22)" ] 
-cuts["SR3"] += ["(abs(H2Pa/H3Pa - H2Pb/H3Pb) < 0.5)" ] 
-cuts["SR3"] += ["(  %s > 1800.  )"%var_HT6PP  ]
-cuts["SR3"] += ["(  H2PP > 700.  )"  ]
-cuts["SR3"] += ["(  min(H3Pa,H3Pb) > 500. )"  ]
-cuts["SR3"] += ["(  min(pTPP_jet2a,pTPP_jet2b)/%s > 0.06 )"%var_HT6PP  ]
+cuts["SR3"] += ["( pT_jet1 > 50.)"]
+cuts["SR3"] += ["( pT_jet2 > 50.)"]
+cuts["SR3"] += ["( MDR > 300.)"]
+cuts["SR3"] += ["( deltaQCD/(Rsib-1) < 0.05)"]
+cuts["SR3"] += ["( RPT < 0.4)"]
+cuts["SR3"] += ["( RPZ_HT6PP < 0.55)"]
+cuts["SR3"] += ["( sangle < 0.3)" ]
+cuts["SR3"] += ["( R_H2PP_H6PP > 0.23)" ] 
+cuts["SR3"] += ["( R_HT6PP_H6PP > 0.55)" ] 
+cuts["SR3"] += ["( minH3P/H6PP > 0.22)" ] 
+cuts["SR3"] += ["( abs(dH2o3P) < 0.5)" ] 
+cuts["SR3"] += ["( HT6PP > 1800.  )"  ]
+cuts["SR3"] += ["( H2PP > 700.  )"  ]
+cuts["SR3"] += ["( minH3P > 500. )"  ]
+cuts["SR3"] += ["( min(R_pTj2a_HT6PP,R_pTj2b_HT6PP) > 0.06 )"  ]
 
 
 limits["SR3"] = []
@@ -199,20 +191,20 @@ limits["SR3"] += [(50,0,0.2)]  #["(  min(pTPP_jet2a,pTPP_jet2b)/%s > 0.06 )"%var
 
 
 cuts["SR4"] = []
-cuts["SR4"] += ["(pT_jet1 > 50.)"]
-cuts["SR4"] += ["(pT_jet2 > 50.)"]
-cuts["SR4"] += ["(MDR > 300.)"]
-cuts["SR4"] += ["(deltaQCD/(Rsib-1) < 0.05)"]
-cuts["SR4"] += ["(RPT < 0.4)"]
-cuts["SR4"] += ["(%s < 0.55)"%var_RPZ]
-cuts["SR4"] += ["(abs(cosP+%s)/2 < 0.6)"%var_dphiVP]
-cuts["SR4"] += ["((H2PP / H6PP) > 0.24)" ] 
-cuts["SR4"] += ["(  min(H3Pa,H3Pb)/H6PP > 0.22)" ] 
-cuts["SR4"] += ["(abs(H2Pa/H3Pa - H2Pb/H3Pb) < 0.5)" ] 
-cuts["SR4"] += ["(  %s > 1900.  )"%var_HT6PP  ]
+cuts["SR4"] += ["( pT_jet1 > 50.)"]
+cuts["SR4"] += ["( pT_jet2 > 50.)"]
+cuts["SR4"] += ["( MDR > 300.)"]
+cuts["SR4"] += ["( deltaQCD/(Rsib-1) < 0.05)"]
+cuts["SR4"] += ["( RPT < 0.4)"]
+cuts["SR4"] += ["( RPZ_HT6PP < 0.55)"]
+cuts["SR4"] += ["( sangle < 0.6)"]
+cuts["SR4"] += ["( R_H2PP_H6PP > 0.24)" ] 
+cuts["SR4"] += ["( minH3P/H6PP > 0.22)" ] 
+cuts["SR4"] += ["( abs(dH2o3P)  < 0.5)" ] 
+cuts["SR4"] += ["(  HT6PP > 1900.  )"  ]
 cuts["SR4"] += ["(  H2PP > 650.  )"  ]
-cuts["SR4"] += ["(  min(H3Pa,H3Pb) > 600. )"  ]
-cuts["SR4"] += ["(  min(pTPP_jet2a,pTPP_jet2b)/%s > 0.06 )"%var_HT6PP  ]
+cuts["SR4"] += ["(  minH3P > 600. )"  ]
+cuts["SR4"] += ["(  min(R_pTj2a_HT6PP,R_pTj2b_HT6PP) > 0.06 )"  ]
 
 
 limits["SR4"] = []
@@ -233,20 +225,20 @@ limits["SR4"] += [(50,0,0.2)]  #["(  min(pTPP_jet2a,pTPP_jet2b)/%s > 0.06 )"%var
 
 
 cuts["SR5"] = []
-cuts["SR5"] += ["(pT_jet1 > 50.)"]
-cuts["SR5"] += ["(pT_jet2 > 50.)"]
-cuts["SR5"] += ["(MDR > 300.)"]
-cuts["SR5"] += ["(deltaQCD/(Rsib-1) < 0.05)"]
-cuts["SR5"] += ["(RPT < 0.4)"]
-cuts["SR5"] += ["(%s < 0.55)"%var_RPZ]
-cuts["SR5"] += ["(abs(cosP+%s)/2 < 0.5)"%var_dphiVP]
-cuts["SR5"] += ["(abs(cosP-%s)/2 < 0.875)"%var_dphiVP]
-cuts["SR5"] += ["(  min(H3Pa,H3Pb)/H6PP > 0.2)" ] 
-cuts["SR5"] += ["(abs(H2Pa/H3Pa - H2Pb/H3Pb) < 0.65)" ] 
-cuts["SR5"] += ["(  %s > 2700.  )"%var_HT6PP  ]
-cuts["SR5"] += ["(  H2PP > 900.  )"  ]
-cuts["SR5"] += ["(  min(H3Pa,H3Pb) > 900. )"  ]
-cuts["SR5"] += ["(  min(pTPP_jet2a,pTPP_jet2b)/%s > 0.035 )"%var_HT6PP  ]
+cuts["SR5"] += ["( pT_jet1 > 50.)"]
+cuts["SR5"] += ["( pT_jet2 > 50.)"]
+cuts["SR5"] += ["( MDR > 300.)"]
+cuts["SR5"] += ["( deltaQCD/(Rsib-1) < 0.05)"]
+cuts["SR5"] += ["( RPT < 0.4)"]
+cuts["SR5"] += ["( RPZ_HT6PP < 0.55)"]
+cuts["SR5"] += ["( sangle  < 0.5)"]
+cuts["SR5"] += ["( abs(dangle) < 0.875)"]
+cuts["SR5"] += ["( minH3P/H6PP  > 0.2)" ] 
+cuts["SR5"] += ["( abs(dH2o3P) < 0.65)" ] 
+cuts["SR5"] += ["( HT6PP > 2700.  )"  ]
+cuts["SR5"] += ["( H2PP > 900.  )"  ]
+cuts["SR5"] += ["( minH3P > 900. )"  ]
+cuts["SR5"] += ["( min(R_pTj2a_HT6PP,R_pTj2b_HT6PP) > 0.035 )"  ]
 
 
 limits["SR5"] = []
@@ -256,7 +248,7 @@ limits["SR5"] += [(50,0,2000)]  #["(MDR>300.)"]
 limits["SR5"] += [(50,-1,1)]  #["(deltaQCD>0.05*(1.-Rsib))"]
 limits["SR5"] += [(50,0,1)]  #["(RPT<0.4)"]
 limits["SR5"] += [(50,0,1)]  #["(%s<0.5)"%var_RPZ]
-limits["SR5"] += [(50,0,1)]  #["(abs(cosP+%s)/2<0.5)"%var_dphiVP]
+limits["SR5"] += [(50,0,1)]  #["(abs(cosP+%s)/2<0.5)"]
 limits["SR5"] += [(50,0,1)]  #["(abs(cosP-%s)/2<0.5)"%var_dphiVP]
 limits["SR5"] += [(50,0,1)]  #["((H2PP/%s) < 0.9)"%var_HT6PP ] 
 limits["SR5"] += [(50,0,1)]  #["(abs(H2Pa/H3Pa - H2Pb/H3Pb) < 0.35)" ] 
@@ -303,7 +295,7 @@ for SH_name, mysamplehandler in my_SHs.iteritems():
 			for i,cutpart in enumerate(cuts[region]):
 
 				cutpartname = cutpart.translate(None, " (),.").replace("*","_x_").replace("/","_over_").split(" < ")[0].split(" > ")[0]
-				variablename = cutpart.split(" < ")[0].split(" > ")[0]+")"
+				variablename = cutpart.split("<")[0].split(">")[0]+")"
 
 				# print variablename
 				job.algsAdd (ROOT.MD.AlgHist(ROOT.TH1F("%s_minus_%s"%(region,cutpartname), "%s_%s"%(region,cutpartname), limits[region][i][0], limits[region][i][1], limits[region][i][2] ), variablename ,"weight*%s"%"*".join(["(%s)"%mycut for mycut in cuts[region] if mycut!=cutpart ])    )        )
@@ -321,9 +313,9 @@ for SH_name, mysamplehandler in my_SHs.iteritems():
 		job.algsAdd (ROOT.MD.AlgHist(ROOT.TH1F("H2PP_%s"%region, "H2PP_%s"%region, 100, 0, 2000), 
 			"H2PP",
 			"weight*%s"%regions[region]))
-		job.algsAdd (ROOT.MD.AlgHist(ROOT.TH1F("pTPP_jet2_over_HT6PP_%s"%region, "pTPP_jet2_over_HT6PP_%s"%region, 100, 0, 0.1), 
-			"( min(pTPP_jet2a,pTPP_jet2b)/%s )"%var_HT6PP,
-			"weight*%s"%regions[region]))
+		# job.algsAdd (ROOT.MD.AlgHist(ROOT.TH1F("pTPP_jet2_over_HT6PP_%s"%region, "pTPP_jet2_over_HT6PP_%s"%region, 100, 0, 0.1), 
+		# 	"( min(pTPP_jet2a,pTPP_jet2b)/%s )"%var_HT6PP,
+		# 	"weight*%s"%regions[region]))
 
 
 
