682bc3df4b1a0dfba4c0fd318eb44d86bbabf559
Teng Jian Khoo <khoo@pcka.hep.phy.cam.ac.uk>
2016-01-21 17:17:09 +0000

    zll ratios

--- extract_Zg_ratio.py
+++ extract_Zg_ratio.py
@@ -6,44 +6,56 @@ rwvars = ['met','bosonPt','bosonEt','bosonEta','dPhi']
 outfile = ROOT.TFile('ratZG.root','RECREATE')
 for datasource in ['reco','truth']:
     outfile.mkdir(datasource)
-    z_in = ROOT.TFile('rundir_z_lo_'+datasource+'.root') if datasource == 'truth' else ROOT.TFile('rundir_z_nlo_'+datasource+'.root')
+    zvv_in = ROOT.TFile('rundir_zvv_lo_'+datasource+'.root') if datasource == 'truth' else ROOT.TFile('rundir_zvv_nlo_'+datasource+'.root')
+    zll_in = ROOT.TFile('rundir_zll_lo_'+datasource+'.root') if datasource == 'truth' else ROOT.TFile('rundir_zll_nlo_'+datasource+'.root')
     g_in = ROOT.TFile('rundir_gamma_'+datasource+'.root')
 
     for level in cutlevels:
         for rwvar in rwvars:
-            zhist = z_in.Get(rwvar+'_'+level)
+            zvvhist = zvv_in.Get(rwvar+'_'+level)
             ghist = g_in.Get(rwvar+'_'+level)
-            rat = zhist.Clone('Rzg_{0}_{1}'.format(rwvar,level))
+
+            print zvvhist, ghist
+            rat = zvvhist.Clone('Rzvvg_{0}_{1}'.format(rwvar,level))
             rat.Divide(ghist)
             #rat.Scale(1./math.sqrt(rat.Integral()))
             outfile.cd(datasource)
             rat.Write()
+
+            if datasource=='reco':
+                zllhist = zll_in.Get(rwvar+'_'+level)
+                print zllhist, ghist
+                rat2 = zllhist.Clone('Rzllg_{0}_{1}'.format(rwvar,level))
+                rat2.Divide(ghist)
+                #rat2.Scale(1./math.sqrt(rat2.Integral()))
+                outfile.cd(datasource)
+                rat2.Write()
 
         for rwpair in [('bosonPt','dPhi'),('bosonEt','dPhi')]:
-            zhist = z_in.Get(rwpair[0]+'_'+rwpair[1]+'_'+level)
+            zvvhist = zvv_in.Get(rwpair[0]+'_'+rwpair[1]+'_'+level)
             ghist = g_in.Get(rwpair[0]+'_'+rwpair[1]+'_'+level)
-            rat = zhist.Clone('Rzg_{0}_{1}_{2}'.format(rwpair[0],rwpair[1],level))
+
+            rat = zvvhist.Clone('Rzvvg_{0}_{1}'.format(rwvar,level))
             rat.Divide(ghist)
             #rat.Scale(1./math.sqrt(rat.Integral()))
             outfile.cd(datasource)
             rat.Write()
 
-        reweighthist1 = outfile.Get(datasource+'/Rzg_bosonPt_'+level)
-        reweighthist2 = outfile.Get(datasource+'/Rzg_dPhi_'+level)
-        #reweighthist2.Scale(1./reweighthist2.Integral())
-        reweighthist = ROOT.TH2D('Rzg_bosonPt_dPhi_'+level+'_alt','Rzg_bosonPt_dPhi_'+level+'_alt',
-                                 reweighthist1.GetNbinsX(),reweighthist1.GetXaxis().GetXmin(),reweighthist1.GetXaxis().GetXmax(),
-                                 reweighthist2.GetNbinsX(),reweighthist2.GetXaxis().GetXmin(),reweighthist2.GetXaxis().GetXmax())
-        for ibin in range(1,reweighthist1.GetNbinsX()+2):
-            for jbin in range(1,reweighthist2.GetNbinsX()+1):
-                reweighthist.SetBinContent(ibin,jbin,reweighthist1.GetBinContent(ibin)*reweighthist2.GetBinContent(jbin)*reweighthist2.GetNbinsX()/reweighthist2.Integral())
-        reweighthist.Write()
+            if datasource=='reco':
+                zllhist = zll_in.Get(rwpair[0]+'_'+rwpair[1]+'_'+level)
+                ghist = g_in.Get(rwpair[0]+'_'+rwpair[1]+'_'+level)
+
+                rat2 = zllhist.Clone('Rzvvg_{0}_{1}_{2}'.format(rwpair[0],rwpair[1],level))
+                rat2.Divide(ghist)
+                #rat.Scale(1./math.sqrt(rat.Integral()))
+                outfile.cd(datasource)
+                rat2.Write()
 
 effvars = ['bosonPt','bosonEta']
 outfile.mkdir('efficiency')
-for process in ['z','gamma']:
-    reco_in = {'z':ROOT.TFile('rundir_z_nlo_reco.root'),'gamma':ROOT.TFile('rundir_gamma_reco.root')}[process]
-    truth_in = {'z':ROOT.TFile('rundir_z_nlo_truth.root'),'gamma':ROOT.TFile('rundir_gamma_truth.root')}[process]
+for process in ['zvv','gamma']:#'zll'
+    reco_in = {'zvv':ROOT.TFile('rundir_zvv_nlo_reco.root'),'zll':ROOT.TFile('rundir_zll_nlo_reco.root'),'gamma':ROOT.TFile('rundir_gamma_reco.root')}[process]
+    truth_in = {'zvv':ROOT.TFile('rundir_zvv_nlo_truth.root'),'zll':ROOT.TFile('rundir_zll_nlo_truth.root'),'gamma':ROOT.TFile('rundir_gamma_truth.root')}[process]
     for level in cutlevels:
         for effvar in effvars:
             recohist = reco_in.Get(effvar+'_'+level)
