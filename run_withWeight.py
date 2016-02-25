6d8adabe717ca972d18d6c1439b4668577975633
Russell Smith <rsmith@cern.ch>
2016-02-25 13:16:42 -0500

    adding rw and not rw photon

--- run_withWeight.py
+++ run_withWeight.py
@@ -2,12 +2,12 @@ import ROOT
 import os
 import string
 import discoverInput
+import variableNames
+import cutNames
 import copy
 
-def cuts_from_dict(cutdict):
-    return "*".join( ["(%s)"%mycut for mycut in cutdict.keys() ])
-
 ROOT.gROOT.SetBatch(True)
+
 
 from optparse import OptionParser
 parser = OptionParser()
@@ -16,7 +16,9 @@ parser.add_option('--reweightCuts'       , help='cuts used to derive ratio', cho
 parser.add_option('--reweightDataSource' , help='reweight by truth with reco efficiency or directly from reco', choices=('truth','reco'), default='truth')
 parser.add_option('--reweightHists'      , help='reweight in which variables', choices=('bosonPt_dPhi','bosonEt_dPhi','Nj50_dPhi'),default='bosonPt_dPhi')
 parser.add_option('--doZnunuEffWeight'   , help='Don\'t assume that Znunu have reco eff of 1 .', action="store_true", default=False)
+parser.add_option("--isTest", action="store_true", default=False)
 (options, args) = parser.parse_args()
+
 
 #gamma_lo_truth  gamma_reco  z_lo_truth  z_nlo_truth  z_reco
 
@@ -51,21 +53,14 @@ for sample in sh_all:
         sh_bg[sample.name()] = sample_gamma
 
 print sh_bg
-
-no_cuts = {}
-no_cuts["1"] = [10,0,1000]
-cuts = {
-    "no_cuts"     : cuts_from_dict(no_cuts),
-    }
-
-#reweightvars  = ['dPhi']
+reweightvars  = ['dPhi']
 #reweightHists = {}
 #somehow do a list of vars you want to create weights for
 #for rwvar in reweightvars :
 #    reweightHists[rwvar] = getWeightHistogram(mytrees['gamma'],mytrees['zvv'], rwvar , "1.*(dPhi<4.)")
 
 for samplename, sample in sh_bg.iteritems() :
-#    if not (samplename == 'gamma_lo_truth') : continue
+    if not (samplename == 'gamma_lo_truth') : continue
 
     sh = ROOT.SH.SampleHandler()
     sh.add(sample)
@@ -73,15 +68,27 @@ for samplename, sample in sh_bg.iteritems() :
     job = ROOT.EL.Job()
     job.sampleHandler(sh)
 
+    count = 0
 
-    limits = (100, 0, 1000)
-    for cut in cuts :
-        for varname in ['met'] :
+    for cutname,cut in cutNames.cuts.iteritems() :
+        count = count + 1
+        for varname,limits in variableNames.RJigsawVariables.iteritems() :
             weight = 'normweight*(NTVars.eventWeight)'
-            cutstring = "weight_RZvvG*NTVars.eventWeight*normweight*(%s)"%cuts[cut]
-            thehist = ROOT.TH1D(varname+"_%s"%cut, varname+"_%s"%cut,
+            cutstring = weight+"*(%s)"%cut
+            thehist = ROOT.TH1D('_'.join([varname,"%s"%cutname]),
+                                '_'.join([varname,"%s"%cutname]),
                                 limits[0], limits[1], limits[2])
             job.algsAdd(ROOT.MD.AlgHist(thehist,varname,cutstring))
+
+            if 'gamma' in samplename :
+                for zproc in ['Zll','Zvv'] :
+                    for rwvar in ['dPhi'] :
+                        cutstring = "weight_R"+zproc+"G*NTVars.eventWeight*normweight*(%s)"%cut
+                        thehist = ROOT.TH1D('_'.join([varname,zproc,rwvar,"%s"%cutname]),
+                                            '_'.join([varname,zproc,rwvar,"%s"%cutname]),
+                                            limits[0], limits[1], limits[2])
+                        job.algsAdd(ROOT.MD.AlgHist(thehist,varname,cutstring))
+            if (options.isTest and  count > 1) : break
 
     driver = None
     if options.driver == "prooflite" :
@@ -89,13 +96,13 @@ for samplename, sample in sh_bg.iteritems() :
         driver.numWorkers = 3
     elif options.driver == "lsf" :
         driver = ROOT.EL.LSFDriver()
-        ROOT.SH.scanNEvents(process);
-        process.setMetaDouble(ROOT.EL.Job.optEventsPerWorker, 100000);
+        ROOT.SH.scanNEvents(sh);
+        sh.setMetaDouble(ROOT.EL.Job.optEventsPerWorker, 100000);
         job.options().setString(ROOT.EL.Job.optSubmitFlags, "-q " + "1nh");
     elif options.driver == "condor":
         driver = ROOT.EL.CondorDriver()
-        ROOT.SH.scanNEvents(process);
-        process.setMetaDouble(ROOT.EL.Job.optEventsPerWorker, 100000);
+        ROOT.SH.scanNEvents(sh);
+        sh.setMetaDouble(ROOT.EL.Job.optEventsPerWorker, 100000);
         driver.options().setString(ROOT.EL.Job.optCondorConf, "notification=never");
         if "bnl" in os.getenv("HOSTNAME") : job.options().setString (ROOT.EL.Job.optCondorConf, "accounting_group = group_atlas.general");
 
