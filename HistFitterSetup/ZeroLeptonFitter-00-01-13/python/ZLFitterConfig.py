a00ea4a3ef0a37a2b99a2b61f71a1ae41719559a
Russell Smith <rsmith@cern.ch>
2016-07-25 22:11:27 -0400

    updating lumi

--- HistFitterSetup/ZeroLeptonFitter-00-01-13/python/ZLFitterConfig.py
+++ HistFitterSetup/ZeroLeptonFitter-00-01-13/python/ZLFitterConfig.py
@@ -122,8 +122,8 @@ class ZLFitterConfig:
         # Luminosity
         ##############################################
 
-        self.luminosity = 11.3 # 2.674#unit is fb-1
-        self.luminosityEr = 0.037 # style the run1 error ####################
+        self.luminosity = 13.28 # 2.674#unit is fb-1
+        self.luminosityEr = 0.029 # style the run1 error ####################
 
         ##############################################
         # samples
