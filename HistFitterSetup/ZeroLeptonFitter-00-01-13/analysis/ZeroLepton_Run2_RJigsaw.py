9f73b758d440079e21f6ad8fa7211e30f7846d09
Russell Smith <rsmith@cern.ch>
2016-07-27 12:33:52 -0400

    changing data name

--- HistFitterSetup/ZeroLeptonFitter-00-01-13/analysis/ZeroLepton_Run2_RJigsaw.py
+++ HistFitterSetup/ZeroLeptonFitter-00-01-13/analysis/ZeroLepton_Run2_RJigsaw.py
@@ -294,7 +294,7 @@ if configMgr.readFromTree:
     # dataFiles.append(INPUTDIR_DATA+ "/Data_Nov07.root")
     # dataFiles.append(INPUTDIR_DATA+ "/DataMain_data15_13TeV.root")
     # dataFiles.append(INPUTDIR_DATA+ "/DataMain_data16_13TeV.root")
-    dataFiles.append(INPUTDIR_DATA+ "/DataMain_303560.root")
+    dataFiles.append(INPUTDIR_DATA+ "/DataMain.root")
 
     log.info("Using the following inputs:")
     log.info("topFiles = %s" % topFiles)
